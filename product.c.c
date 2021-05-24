95d1b2d5cff4ad35a441d9cadfaebdd8c6778e1b
karthikmarella24 <80272758+karthikmarella24@users.noreply.github.com>
2021-05-24 19:31:05 +0530

    Add files via upload

---
+++ product.c.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,product;
+	printf("Enter a two numbers:\n");
+	scanf("%d%d",&num1,&num2);
+	product=num1*num2;
+	printf("product of two numbers:%d",product);
+	return 0;
+}
