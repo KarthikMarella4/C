bc105fc50b984031c1baf43bfc890b9de408e096
karthikmarella24 <80272758+karthikmarella24@users.noreply.github.com>
2021-05-24 19:30:43 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  int b;
+     printf("Enter the side");
+     scanf("%d",&a);
+     b=a*a;
+     printf("The area of requried square is:%d",b);
+     return 0;
+}
